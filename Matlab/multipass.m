4e75db29ed11cbba766a673b1b21931185f97eb7
binarymillenium <binarymillenium@cf34886a-44c1-11de-a1df-459bf3fe8569>
2010-01-17 19:46:09 +0000

    Current values are for my 'jess' dataset.  The median of multiple phases produces good results, but it seems like it is only a crude filter- something more intelligent would weight different phases differently for different feature types.

--- Matlab/multipass.m
+++ Matlab/multipass.m
@@ -15,6 +15,7 @@ angles(1,:) = angles1;
 unwrapped_angles = zeros(5,len1);
 uangles = unwrapped_angles;
 unwrapped_angles(1,:) = unwrapped_angles1;
+unwrapped_angles_norm = unwrapped_angles;
 
 
 [t,angles(2,:),unwrapped_angles(2,:),pd]=structuredlight('../Processing/SimThreePhase/phase1004.jpg','../Processing/SimThreePhase/phase1005.jpg','../Processing/SimThreePhase/phase1006.jpg', '../Processing/SimThreePhase/heightdata.dat');
@@ -22,23 +23,36 @@ unwrapped_angles(1,:) = unwrapped_angles1;
 [t,angles(4,:),unwrapped_angles(4,:),pd]=structuredlight('../Processing/SimThreePhase/phase1010.jpg','../Processing/SimThreePhase/phase1011.jpg','../Processing/SimThreePhase/phase1012.jpg', '../Processing/SimThreePhase/heightdata.dat');
 [t,angles(5,:),unwrapped_angles(5,:),pd]=structuredlight('../Processing/SimThreePhase/phase1013.jpg','../Processing/SimThreePhase/phase1014.jpg','../Processing/SimThreePhase/phase1015.jpg', '../Processing/SimThreePhase/heightdata.dat');
 
+%%
+% increasing skew will tend to tilt the unwrapped angles to the left,
+% decreasing will tilt to the right
+skew = 0.9465;
+scale = 2900;
+
 flat_angles = [1:len1]/len1;
-flat_angles = flat_angles*1.73+1;
+flat_angles = flat_angles - flat_angles(end/2);
+flat_angles = flat_angles*skew;
 
 maxi = size(angles,1);
 for i = [1:maxi]
-    uangles(i,:) = unwrapped_angles(i,:)/(2^(i-1)) - flat_angles;
+    uangles(i,:) = scale*(unwrapped_angles(i,:)/(2^(i-1)) - flat_angles);
     uangles(i,:) = uangles(i,:) - uangles(i,end/2);
-    unwrapped_angles(i,:) = unwrapped_angles(i,:); %/(2^(i-1));
+    unwrapped_angles_norm(i,:) = unwrapped_angles(i,:)/(2^(i-1));
     
-    angles(i,:) = angles(i,:)+i*1.2;
+    staggered_angles(i,:) = angles(i,:)+i*1.2;
 end
 
-
-
 figure(1);
-subplot(2,1,1),plot(t,angles);
+subplot(2,1,1),plot(t,staggered_angles);
 subplot(2,1,2),plot(t,unwrapped_angles);
 
 figure(2);
-plot(t(end/4:end-end/4),uangles(:,end/4:end-end/4),t(end/4:end-end/4),pd(end/4:end-end/4)/(13.0*max(max(abs(pd(end/4:end-end/4))))));
+plot(t, flat_angles, t, unwrapped_angles_norm);
+
+figure(3);
+plot(t(end/4:end-end/4),uangles(:,end/4:end-end/4),t(end/4:end-end/4),pd(end/4:end-end/4) );
+axis([1*size(angles,2)/4 3*size(angles,2)/4 -100 max(pd(end/4:end-end/4))]); %min(pd(end/4:end-end/4))
+
+figure(4)
+plot(t(end/4:end-end/4),median(uangles(:,end/4:end-end/4),1), t(end/4:end-end/4), pd(end/4:end-end/4) );
+%axis([1*size(angles,2)/4 3*size(angles,2)/4 -10 max(pd(end/4:end-end/4))]); %min(pd(end/4:end-end/4))
