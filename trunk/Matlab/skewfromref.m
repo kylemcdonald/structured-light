8b90dfb62edd6e2d25e1bf6e14ac02424df87351
binarymillenium <binarymillenium@cf34886a-44c1-11de-a1df-459bf3fe8569>
2010-01-23 18:18:48 +0000

    Still trying to automatically determine skew from reference images, but it isn't working right- it may generate a good starting skew but would still have to be adjusted manually.

--- trunk/Matlab/skewfromref.m
+++ trunk/Matlab/skewfromref.m
@@ -1,4 +1,4 @@
-function [skew, scale, incr,uangle] = skewfromref(refname,targsize,div)
+function [skew, scale, period, uangle] = skewfromref(refname,targsize,div)
 % compute the skew, scale, and angle from the ref image and its known size
 % (as measured from the center to the edge)
 p1 = rot90(double( sum(imread(refname),3)/3));
@@ -9,28 +9,34 @@ p1 = p1(end/2, :);
 % be a lot a smaller
 ang=2*(p1/max(p1)-0.5);
 angle = acos(ang)/pi;
-figure(30), plot(ang);
+zerocrosses = ((ang(1:end-1) < 0) .* (ang(2:end) >=0));
+figure(30), plot([1:length(ang)],ang, [1:length(zerocrosses)], zerocrosses);
+period = mean(diff(find(zerocrosses)));
 
 
-% this only works okay, might need a full three phase on the ref
-% or just count the cycles present
-incr = abs(diff(angle));
-uangle = incr;
-%uangle(end/2) = 0;
+uangle = (([1:length(ang)]-length(ang)/2)/period);
+skew = 2*(max(uangle)/div);
+scale = 1;
 
-uangle(end/2+1:end) = cumsum(incr(end/2+1:end));
-uangle(1:end/2)  = -fliplr(cumsum(fliplr(incr(1:end/2))));
-uangle(1:end/2) = uangle(1:end/2) + -uangle(floor(end/2))
-
-%angle = angle/div;
-
-maxangle = max(uangle);
-pixmax = length(uangle)/2;
-pixend = min(find(uangle == maxangle))-pixmax;
-uangle = uangle*pixend/pixmax;
-
-skew = 0;
-scale = 0;
+% % this only works okay, might need a full three phase on the ref
+% % or just count the cycles present
+% incr = abs(diff(angle));
+% uangle = incr;
+% %uangle(end/2) = 0;
+% 
+% uangle(end/2+1:end) = cumsum(incr(end/2+1:end));
+% uangle(1:end/2)  = -fliplr(cumsum(fliplr(incr(1:end/2))));
+% uangle(1:end/2) = uangle(1:end/2) + - uangle(floor(end/2));
+% 
+% %angle = angle/div;
+% 
+% maxangle = max(uangle);
+% pixmax = length(uangle)/2;
+% pixend = min(find(uangle == maxangle))-pixmax;
+% uangle = uangle*pixend/pixmax;
+% 
+% skew = maxangle*(pixend/pixmax)/div;
+% scale = 0;
 
 
 
